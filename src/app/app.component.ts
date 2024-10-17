a74dd8412d2f8ff3d70280f9979c56801e520fae
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-10-17 21:35:25 +0200

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CanvasComponent } from './canvas/canvas.component';
+import { EditorComponent } from './editor/editor.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CanvasComponent, EditorComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Haunted Canvas';
+  title: string = 'Haunted Canvas';
+  greenEyeUrl: string = 'assets/green-eye.png';
 }
