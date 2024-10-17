878115f9aa51192c4e3a36e82f74cd448a3a0e72
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-10-17 21:34:09 +0200

    create canvas component

---
+++ src/app/canvas/canvas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'editor-canvas',
+  standalone: true,
+  imports: [],
+  templateUrl: './canvas.component.html',
+  styleUrl: './canvas.component.css',
+})
+export class CanvasComponent {}
