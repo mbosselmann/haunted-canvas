0b83f1f71e6099ed077e9337efd1c9e3f4d0e635
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-10-21 22:26:59 +0200

    implement editor control sections

--- src/app/editor-controls/editor-controls.component.ts
+++ src/app/editor-controls/editor-controls.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CategoryOptions } from '../model/categoryOption';
+import { categoryOptions } from '../data/categoryOptions';
 
 @Component({
   selector: 'editor-controls',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
   templateUrl: './editor-controls.component.html',
   styleUrl: './editor-controls.component.css',
 })
-export class EditorControlsComponent {}
+export class EditorControlsComponent {
+  categories: string[] = ['settings', 'filter', 'sticker', 'save'];
+  selectedCategory: string = '';
+  selectedOption: string = '';
+
+  categoryOptions: CategoryOptions = categoryOptions;
+
+  onSelectCategory(category: string) {
+    this.selectedCategory = this.selectedCategory === category ? '' : category;
+  }
+  onSelectOption(option: string) {
+    this.selectedOption = this.selectedOption === option ? '' : option;
+    this.categoryOptions[this.selectedCategory]
+      .find((option) => option.name === this.selectedOption)
+      ?.action();
+  }
+}
