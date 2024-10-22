907646513bb5ab0bed396fcbfdada67ab9081173
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-10-22 23:13:14 +0200

    update canvas component to render selected image

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'editor-canvas',
@@ -7,4 +13,39 @@ import { Component } from '@angular/core';
   templateUrl: './canvas.component.html',
   styleUrl: './canvas.component.css',
 })
-export class CanvasComponent {}
+export class CanvasComponent implements AfterViewInit {
+  @Input('previewImage')
+  previewImage!: string;
+
+  @ViewChild('canvas', { read: ElementRef })
+  canvas!: ElementRef<HTMLCanvasElement>;
+
+  loadImageToCanvas(imageUrl: string) {
+    const canvas = this.canvas.nativeElement;
+    const ctx = canvas.getContext('2d');
+    if (ctx) {
+      const image = new Image();
+      image.onload = () => {
+        const aspectRatio = image.naturalWidth / image.naturalHeight;
+        const targetWidth = 375;
+        const targetHeight = 420;
+
+        if (image.naturalWidth > image.naturalHeight) {
+          canvas.width = targetWidth;
+          canvas.height = targetWidth / aspectRatio;
+        } else {
+          canvas.height = targetHeight;
+          canvas.width = targetHeight * aspectRatio;
+        }
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+      };
+      image.src = imageUrl;
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.loadImageToCanvas(this.previewImage);
+  }
+}
