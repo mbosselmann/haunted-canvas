e4907647e9c9b86a66d4c751b348207441b42f91
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-10-22 23:12:14 +0200

    create image upload form component

---
+++ src/app/image-upload-form/image-upload-form.component.ts
@@ -0,0 +1,36 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'image-upload',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './image-upload-form.component.html',
+  styleUrl: './image-upload-form.component.css',
+})
+export class ImageUploadComponent {
+  imageUploadControl = new FormControl();
+  payLoad = '';
+
+  @Output()
+  onImageIsChosen = new EventEmitter<boolean>();
+
+  @Output()
+  setPreviewImage = new EventEmitter<string>();
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+  }
+
+  onFileChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files?.[0]) return;
+
+    if (!input.files?.[0]) return;
+    const previewImageUrl = URL.createObjectURL(input.files?.[0]);
+    console.log(previewImageUrl);
+    console.log('File changed!');
+    this.onImageIsChosen.emit(true);
+    this.setPreviewImage.emit(previewImageUrl);
+  }
+}
