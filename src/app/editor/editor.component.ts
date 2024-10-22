74c70e1343ba3772b32760af73b93795d4179a5e
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-10-22 23:12:37 +0200

    update editor component to handle form and canvas

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CanvasComponent } from '../canvas/canvas.component';
 import { EditorControlsComponent } from '../editor-controls/editor-controls.component';
+import { ImageUploadComponent } from '../image-upload-form/image-upload-form.component';
 
 @Component({
   selector: 'editor',
   standalone: true,
-  imports: [CanvasComponent, EditorControlsComponent],
+  imports: [CanvasComponent, EditorControlsComponent, ImageUploadComponent],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.css',
 })
@@ -13,11 +14,23 @@ export class EditorComponent {
   title: string = 'Haunted Canvas';
   closeIconUrl: string = 'assets/close-icon.svg';
   greenEyeUrl: string = 'assets/green-eye.png';
+  isImageChosen: boolean = false;
+  previewImage: any = null;
 
   @Output()
   closeEditor = new EventEmitter<boolean>();
 
   onCloseEditor() {
     this.closeEditor.emit(false);
+    this.isImageChosen = false;
+  }
+
+  onImageChosen(isImageChosen: boolean) {
+    this.isImageChosen = isImageChosen;
+  }
+
+  setPreviewImage(previewImage: string) {
+    this.previewImage = previewImage;
+    console.log(this.previewImage);
   }
 }
