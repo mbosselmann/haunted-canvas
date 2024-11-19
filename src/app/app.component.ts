a5ac7a2fe75006869f9a36ed5393035f9c5fde1d
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2024-11-20 00:03:13 +0100

    remove not needed component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CanvasComponent } from './canvas/canvas.component';
 import { EditorComponent } from './editor/editor.component';
 import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CanvasComponent, EditorComponent, HeroComponent],
+  imports: [RouterOutlet, EditorComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
